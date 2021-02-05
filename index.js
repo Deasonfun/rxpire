00d6e170f69b3ebbf26b56e82ee940c75a38d9ba
deasonfun <dftothemax@gmail.com>
2021-02-05 12:32:18 -0500

    Added main files

---
+++ index.js
@@ -0,0 +1,68 @@
+var express = require ('express');
+var app = express();
+
+var mongoose = require('mongoose');
+
+var server = app.listen(3000, listening);
+
+function listening() {
+    console.log('listening...');
+}
+
+app.use(express.static('public'));
+
+mongoose.connect('mongodb://localhost/admin', function (err) {
+    if (err) throw err;
+    console.log('Database connected...');
+
+    const Schema = mongoose.Schema;
+    const PostSchema = new Schema ({
+        name: String,
+        ndc: Number,
+        lot: String,
+        day: Number,
+        month: Number,
+        year: Number
+    });
+
+    const Model = mongoose.model;
+    const post = Model('Posts', PostSchema);
+
+    app.get('/add/:name/:ndc/:lot/:day/:month/:year?', addPost);
+
+    function addPost (request, response) {
+        var data = request.params;
+        var name = data.name;
+        var ndc = data.ndc;
+        var lot = data.lot;
+        var day = data.day;
+        var month = data.month;
+        var year = data.year;
+
+        console.log(data);
+
+        const newPost = new post ({
+            name: name,
+            ndc: ndc,
+            lot: lot,
+            day: day,
+            month: month,
+            year: year
+        });
+
+        console.log(newPost);
+
+        newPost.save((err, results) => {
+            if (err) throw err;
+            console.log('Data saved...');
+        });
+    }
+
+    app.get('/getdata', (req, res) => {
+        console.log('Getting data...');
+        post.find({}, function (err, data){
+            if (err) throw err;
+            res.send(data);
+        });
+    });
+});
---
+++ public/script.js
@@ -0,0 +1,75 @@
+function setup() {
+  	var button = select('#submit');
+	button.mousePressed(submitRX);
+}
+
+function submitRX() {
+  	var name = select('#name').value();
+  	var ndc = select('#ndc').value();
+  	var lot = select('#lot').value();
+	var day = select('#exDay').value();
+	var month = select('#exMonth').value();
+	var year = select('#exYear').value();
+    
+	loadJSON('add/' + name + '/' + ndc + '/' + lot + '/' + day + '/' + month + '/' + year);
+	location.reload();
+}
+fetch('/getdata', {method: 'GET'})
+    .then(function (response) {
+        if(response.ok) return response.json();
+        throw new Error('Request failed...');
+    })
+    .then(function (data) {
+        console.log("Getting data...");
+        appendData(data);
+    })
+    .catch (function (error) {
+        console.log(error);
+    })
+
+var tableContainer = document.getElementById("mainTable");
+
+function appendData(data) {
+	console.log(data);
+  	for (var i = 0; i <= Object.keys(data).length - 1; i++) {
+		var scriptTableRow = document.createElement("tr");
+    	var name = document.createElement("td");
+    	var ndc = document.createElement("td");
+    	var lot = document.createElement("td");
+		var day = document.createElement("td");
+		var month = document.createElement("td");
+		var year = document.createElement("td");
+    	name.innerHTML = Object.values(data)[i].name;
+    	ndc.innerHTML = Object.values(data)[i].ndc;
+    	lot.innerHTML = Object.values(data)[i].lot;
+		day.innerHTML = Object.values(data)[i].day;
+		month.innerHTML = Object.values(data)[i].month;
+		year.innerHTML = Object.values(data)[i].year;
+    	tableContainer.appendChild(scriptTableRow);
+    	scriptTableRow.appendChild(name);
+    	scriptTableRow.appendChild(ndc);
+    	scriptTableRow.appendChild(lot);
+		scriptTableRow.appendChild(day);
+		scriptTableRow.appendChild(month);
+		scriptTableRow.appendChild(year);
+  	}
+}
+
+function search() {
+	var input, filter, table, tr, td, i, txtValue;
+	input = document.getElementById("search");
+	filter = input.value.toUpperCase();
+	table = document.getElementById("mainTable");
+	tr = table.getElementsByTagName("tr");
+	for (i = 0; i < tr.length; i++) {
+	  	td = tr[i].getElementsByTagName("td")[0];
+	  	if (td) {
+			txtValue = td.textContent || td.innerText;
+			if (txtValue.toUpperCase().indexOf(filter) > -1) {
+		  		tr[i].style.display = "";
+			} else {
+		  		tr[i].style.display = "none";
+			}
+	  	}       
+	}
+}
